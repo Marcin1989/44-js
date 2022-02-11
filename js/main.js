30a5b098485c444b606ae7884842a8883994a92d
marcin1989 <marcinlubanski23@gmail.com>
2022-02-11 09:32:49 +0100

    44-js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let tablicaMarcin = [2, 3, 4, 5, 6];
+
+let tablicaKeo = [108, 22, 55]
+
+function first(zenek) {
+
+    return [ zenek[0], zenek[1] ]
+
+};
+
+
+console.log(first(tablicaMarcin));
+console.log(first(tablicaKeo));
